60f907706182de762f42c2f4a1622bba2e3130c7
ArpedXD <arpedjeyescomen@gmail.com>
2026-04-26 20:39:55 +0800

    test

--- Login/script.js
+++ Login/script.js
@@ -1,4 +1,4 @@
-const BASE = 'https://bend-production-72e5.up.railway.app';
+const BASE = 'https://bend-sprp.onrender.com';
 
 if(localStorage.getItem("login") == "true"){
   window.location.href = 'index.html';
